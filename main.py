b1edd34ba70afbf0b849d701eb136e60c9c0e8db
Nikita Choudhari <choudharinikita96@gmail.com>
2025-02-22 21:20:21 +0530

    Allowed request from all origins

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all origins (or specify your frontend URL)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
